7b6289c73d11765d8794fa97c3736a4cb27ae92d
Rudra Sharma <sharmarudra129@gmail.com>
2026-05-03 10:04:08 +0530

    Finalizing milestone 1 evaluation files, dbt models, and macros

--- dbt_project/models/marts/fct_orders.sql
+++ dbt_project/models/marts/fct_orders.sql
@@ -1,13 +1,19 @@
 {{ config(materialized='incremental',unique_key='order_id') }}
 
-SELECT order_id,customer_id,product_id,order_date,quantity,total_amount 
+SELECT 
+    order_id,
+    customer_id,
+    product_id,
+    order_date,
+    quantity,
+    total_amount 
 FROM {{ ref('int_orders') }}
+WHERE 1=1
 
-{% if is_incremental() %} 
-    {% if var('backfill_start_date', False) and var('backfill_end_date', False) %}
-        WHERE order_date >= '{{ var("backfill_start_date") }}'
-          AND order_date <= '{{ var("backfill_end_date") }}'
-    {% else %}
-        WHERE order_date > (SELECT MAX(order_date) FROM {{ this }})
-    {% endif %}
+{% if var('backfill_start_date', False) and var('backfill_end_date', False) %}
+    AND order_date >= '{{ var("backfill_start_date") }}'
+    AND order_date <= '{{ var("backfill_end_date") }}'
+
+{% elif is_incremental() %}
+    AND order_date > (SELECT MAX(order_date) FROM {{ this }})
 {% endif %}
